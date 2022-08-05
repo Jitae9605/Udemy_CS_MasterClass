1c5e872d74eb7ed3281f3d31402f84af9a92829e
지태 이 <lyj95123@gmail.com>
2022-08-05 12:22:02 +0900

    로그인시스템 추가

---
+++ Udemy_CSharp_MasterClass/Section04_Making_Decisions/if_관리자구분.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04_Making_Decisions
+{
+	class if_관리자구분
+	{
+		static void Main(string[] args)
+		{
+			bool IsAdmin = false;
+			bool isRegistered = true;
+			string userName = "";
+			Console.WriteLine("Please enter your username");
+
+			userName = Console.ReadLine();
+			if(isRegistered && userName != "" && userName.Equals("admin"))
+			{
+				Console.WriteLine("Hi there, registered user");
+				
+				Console.WriteLine("Hi there, " + userName);
+					
+				Console.WriteLine("Hi there, Admin");
+			}
+
+			if(IsAdmin || isRegistered)
+			{
+				Console.WriteLine("You are logeed in");
+			}
+		}
+	}
+}
---
+++ Udemy_CSharp_MasterClass/Section04_Making_Decisions/로그인시스템.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04_Making_Decisions
+{
+	class 로그인시스템
+	{
+		static string username;
+		static string password;
+
+		static void Main(string[] args)
+		{
+			Register();	// 등록
+			Login();	// 등록된아이디를 통한 로그인
+			Console.Read();
+		}
+
+		public static void Register()
+		{
+			Console.WriteLine("Please enter your username");
+			username = Console.ReadLine();	
+			Console.WriteLine("Please enter your password");
+			password = Console.ReadLine();	
+			Console.WriteLine("Registration Complete!");
+
+		}
+
+		public static void Login()
+		{
+			Console.WriteLine("Please enter your username");
+
+			if (username == Console.ReadLine())
+			{
+				Console.WriteLine("Please enter your password");
+				if (password == Console.ReadLine())
+				{
+					Console.WriteLine("Login successful");
+				}
+				else
+				{
+					Console.WriteLine("Login failed, wrong password. Restart Program");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Login failed, wrong username. Restart Program");
+			}
+		}
+
+	}
+}
