88cfa98aadded0ff9d03015c588854bd0cc2792a
Jitae9605 <dlwlxo3819@naver.com>
2022-08-10 11:06:32 +0900

    누락된 파일 추가

---
+++ Udemy_CSharp_MasterClass/Section04_Making_Decisions/if_else_TryParse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04_Making_Decisions
+{
+	class if_else_TryParse
+	{
+		static void Main(string[] args)
+		{
+			string tempstring = Console.ReadLine();
+			int temp, num;
+
+			if(int.TryParse(tempstring, out num))
+			{
+				temp = num;
+			}
+			else
+			{
+				temp = 0;
+			}
+
+			if(temp < 10)
+			{
+				Console.WriteLine("take the coat");
+			}
+			else if(temp == 10)
+			{
+				Console.WriteLine("10 degrees");
+			}
+			else
+			{
+				Console.WriteLine("warm");
+			}
+
+			
+			Console.Read();
+		}
+	}
+}
